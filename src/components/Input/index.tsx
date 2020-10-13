46eea402eea6952ab88e5ed49a2ad14b54a60873
Vlad <vlad.nifos@gmail.com>
2020-10-13 08:02:09 +0300

    Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  value: string;
+  name: string;
+  onChange: (value: string, name: string) => void;
+  placeholder?: string;
+  type?: string;
+  fullwidth?: boolean;
+  invalid?: boolean;
+}
+
+export function Input(props: Props) {
+  const invalidClasses = props.invalid ? "border-2 border-red-400" : "";
+  const fullwidthClasses = props.fullwidth ? "min-w-full" : "";
+  return (
+    <input
+      value={props.value}
+      type={props.type}
+      placeholder={props.placeholder}
+      className={`${invalidClasses} ${fullwidthClasses} focus:outline-none focus:bg-white focus:shadow-outline focus:border-green-400 `}
+      onChange={(e) => props.onChange(e.target.value, props.name)}
+    />
+  );
+}
